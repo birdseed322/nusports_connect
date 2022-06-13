341e2ae384fcef2052f2d6c53345318a60b25af9
Wesley Teo <wesley.tyx@gmail.com>
2022-06-14 01:04:21 +0800

    Edited Sessions page with dummy code and no functionality

--- nusportsconnect_front/src/Components/Event/Sessions.jsx
+++ nusportsconnect_front/src/Components/Event/Sessions.jsx
@@ -2,13 +2,86 @@ import React from "react";
 import Navbar from "../NavBar/Navbar";
 import FilterBar from "./FilterBar";
 import "./sessionStyles.css";
+import { DateTime } from "luxon";
+import EventPillHost from "../EventPill/EventPillHost";
 
 function Sessions() {
+  //dummy code for events
+  const user = {
+    name: "Samuel Tay",
+    email: "someemail@gmail.com",
+    rating: 4.6,
+    creationDate: "20/02/22",
+    sportingInterests: ["Tennis", "Ultimate Frisbee"],
+  };
+
+  const events = [
+    {
+      eventName: "Badminton",
+      eventLocation: "UTSH2",
+      eventDate: DateTime.local(2022, 2, 21).toLocaleString(),
+      eventStart: "9am",
+      eventEnd: "11am",
+      eventMembers: ["Wesley Teo", "Ezekiel Ang", "Jeremiah Lim"],
+      eventCurrentPax: 3,
+      eventMaxPax: 4,
+      eventHost: "Peter Tan",
+    },
+    {
+      eventName: "Badminton",
+      eventLocation: "UTSH2",
+      eventDate: DateTime.local(2022, 2, 21).toLocaleString(),
+      eventStart: "9am",
+      eventEnd: "11am",
+      eventMembers: ["Wesley Teo", "Ezekiel Ang", "Samuel Tay"],
+      eventCurrentPax: 3,
+      eventMaxPax: 4,
+      eventHost: "Peter Tan",
+    },
+    {
+      eventName: "Badminton",
+      eventLocation: "UTSH2",
+      eventDate: DateTime.local(2022, 2, 22).toLocaleString(),
+      eventStart: "9am",
+      eventEnd: "11am",
+      eventMembers: ["Wesley Teo", "Ezekiel Ang", "Samuel Tay"],
+      eventCurrentPax: 3,
+      eventMaxPax: 4,
+      eventHost: "Peter Tan",
+    },
+  ];
+  // const host = user.name === events.eventHost;
+  // const participant = events.eventMembers.includes(user.name);
   return (
     <div className="sessions-container">
       <Navbar />
       <FilterBar />
-      <div></div>
+      <div className="session">
+        {/* get array of events from database, filter earliest date (lowest month && lowest day?) then wrap it in a div with the date as the heading
+        filter events with same date into a new array, then EventPillHost with their properties.
+
+        Repeat with next date. (Or if database already filters by date then will be best) */}
+        <h1 className="date-header">21st Feb 2022</h1>
+        <EventPillHost
+          // host={host}
+          history={false}
+          participant={user}
+          event={events[0]}
+        />
+        <EventPillHost
+          // host={host}
+          history={false}
+          participant={false}
+          event={events[1]}
+        />
+        <h1 className="date-header">22nd Feb 2022</h1>
+        <EventPillHost
+          // host={host}
+          history={false}
+          participant={false}
+          event={events[2]}
+        />
+      </div>
     </div>
   );
 }
--- nusportsconnect_front/src/Components/Profile/History/ProfileHistoryTab.jsx
+++ nusportsconnect_front/src/Components/Profile/History/ProfileHistoryTab.jsx
@@ -1,27 +1,32 @@
-import React from 'react';
-import EventPillHost from '../../EventPill/EventPillHost';
+import React from "react";
+import EventPillHost from "../../EventPill/EventPillHost";
 
-function ProfileHistoryTab(props){
+function ProfileHistoryTab(props) {
+  //Dummy code. Ideally will pull a list of events associated w member. Then use map function to create pill for each event.
+  const event = {
+    eventName: "Badminton",
+    eventLocation: "UTSH2",
+    eventStart: "9am",
+    eventEnd: "11am",
+    eventMembers: ["Wesley Teo", "Ezekiel Ang", "Samuel Tay"],
+    eventCurrentPax: 3,
+    eventMaxPax: 4,
+    eventHost: "Peter Tan",
+  };
 
-    //Dummy code. Ideally will pull a list of events associated w member. Then use map function to create pill for each event.
-    const event = {
-        eventName : "Badminton",
-        eventLocation : "UTSH2",
-        eventStart : "9am",
-        eventEnd : "11am",
-        eventCurrentPax : 3,
-        eventMaxPax : 4,
-        eventHost : "Peter Tan"
-    }
-
-    //Check if host of event. Should be inside the map function when parsing through list of event.
-    const host = props.user.name === event.eventHost
-
-    return(
-        <div className="profile-tab-info">
-            <EventPillHost host={host} history={true} event={event}/>
-        </div>
-    )
+  //Check if host of event. Should be inside the map function when parsing through list of event.
+  const host = props.user.name === event.eventHost;
+  const participant = event.eventMembers.includes(props.user.name);
+  return (
+    <div className="profile-tab-info">
+      <EventPillHost
+        host={host}
+        history={true}
+        event={event}
+        participant={participant}
+      />
+    </div>
+  );
 }
 
 export default ProfileHistoryTab;
