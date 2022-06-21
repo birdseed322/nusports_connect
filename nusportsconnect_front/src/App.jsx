b8dc7d2d34dcfa94c1397cb8e7b2670cc5b06229
Wesley Teo <wesley.tyx@gmail.com>
2022-06-21 17:26:08 +0800

    Finished create session page. Plugin for time and date input to be confirmed

--- nusportsconnect_front/src/App.jsx
+++ nusportsconnect_front/src/App.jsx
@@ -2,19 +2,21 @@ import React from "react";
 import { setAccessToken } from "./accessToken";
 import EndPoints from "./Routes/EndPoints";
 function App() {
-  const [loading, setLoading] = React.useState(true)
+  const [loading, setLoading] = React.useState(true);
 
-  React.useEffect(()=>{
-    fetch("http://localhost:5000/refresh_token", {credentials:"include", method:'POST'})
-    .then(async x => {
+  React.useEffect(() => {
+    fetch("http://localhost:5000/refresh_token", {
+      credentials: "include",
+      method: "POST",
+    }).then(async (x) => {
       const data = await x.json();
-      setAccessToken(data.accessToken)
-      setLoading(false)
-    })
-  }, [])
+      setAccessToken(data.accessToken);
+      setLoading(false);
+    });
+  }, []);
 
-  if(loading){
-    return <div>loading...</div>
+  if (loading) {
+    return <div>loading...</div>;
   }
 
   return (
