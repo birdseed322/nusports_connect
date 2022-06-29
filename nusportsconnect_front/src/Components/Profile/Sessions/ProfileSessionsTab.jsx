32117501bd9246ff5b7a0e9c8d5467f9e51b8c0a
Ezekiel <ezekielang@protonmail.com>
2022-06-29 17:16:26 +0800

    Added joining function to session details page. Added accurate rendering of joined sessions in profile sessions page.

--- nusportsconnect_front/src/Components/Profile/Sessions/ProfileSessionsTab.jsx
+++ nusportsconnect_front/src/Components/Profile/Sessions/ProfileSessionsTab.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+import { getUserCurrentSessions } from "../../../GraphQLQueries/queries";
 import EventPillHost from "../../EventPill/EventPillHost";
 
 function ProfileSessionsTab(props) {
+  const {id} = useParams()
+  const [data, setData] = React.useState([])
+  React.useEffect(() => {
+    const apiCall = async () => {
+      const sessions = await getUserCurrentSessions(id);
+      setData(sessions.data.data.getUserCurrentSessions);
+    };
+  
+    apiCall();
+  }, [id])
+
+  let uniqDatesSet = new Set();
+  data.forEach((session) => {
+    uniqDatesSet.add(session.date);
+  })
+  let uniqDates = Array.from(uniqDatesSet);
+  uniqDates = uniqDates.sort((a, b) => {
+    return new Date(a) - new Date(b);
+  });
+
   //Dummy code. Ideally will pull a list of events associated w member. Then use map function to create pill for each event.
   const event = {
     id: "62b6b0530eb2c24058edcf78",
@@ -21,7 +43,26 @@ function ProfileSessionsTab(props) {
 
   return (
     <div className="profile-tab-info">
-
+              {uniqDates.map(date => {
+          let toRender = [];
+          for (const session of data) {
+            if (session.date === date){
+              toRender.push(session)
+            }
+          }
+          return(
+            <div><h1>{date}</h1>
+            {toRender.map(session => {
+              return <EventPillHost 
+                history={false}
+                host={host}
+                participant={participant}
+                event={session}
+                />
+            })}
+            </div>
+          )
+  })}
     </div>
   );
 }
