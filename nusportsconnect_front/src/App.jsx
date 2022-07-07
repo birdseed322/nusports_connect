a7e90dddc2b6358a005b0fdcc7ea0fff37ef20f8
unknown <ezekielang@protonmail.com>
2022-07-08 01:06:51 +0800

    bug fix for deployment

--- nusportsconnect_front/src/App.jsx
+++ nusportsconnect_front/src/App.jsx
@@ -6,7 +6,7 @@ import EndPoints from "./Routes/EndPoints";
 function App() {
   const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
-    fetch("http://localhost:5000/refresh_token", {
+    fetch("https://nusportsconnect.herokuapp.com/refresh_token", {
       credentials: "include",
       method: "post",
     }).then(async (x) => {
