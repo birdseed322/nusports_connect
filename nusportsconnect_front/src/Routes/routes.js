7a279ee43595ae3918d67233636ba8a114e494a9
unknown <ezekielang@protonmail.com>
2022-07-08 16:37:55 +0800

    updated package.json

--- nusportsconnect_front/src/Routes/routes.js
+++ nusportsconnect_front/src/Routes/routes.js
@@ -1,5 +1,5 @@
 //Add routes here for organisational purposes. Routes will change once we publish to a hosting service.
-export const loginRoute = "http://localhost:3000";
+// export const loginRoute = "http://localhost:3000";
 //Production
 // export const refreshTokenRoute = "http://localhost:5000/refresh_token";
 // export const reqOriginRoute = "http://localhost:3000";
