8c182260fb0a84cf9809d89ea75afa9403424656
Wesley Teo <wesley.tyx@gmail.com>
2022-07-13 10:58:01 +0800

    Uncommented reviewsLanding

--- nusportsconnect_front/src/Components/Profile/ProfileReviewsLanding.jsx
+++ nusportsconnect_front/src/Components/Profile/ProfileReviewsLanding.jsx
@@ -1,37 +1,40 @@
-// import React from 'react';
-// import {useParams} from 'react-router-dom'
-// import { checkProfileOwner, findUser } from '../../GraphQLQueries/queries';
-// import { Loading } from '../Loading/Loading';
-// import NotAuthenticated from '../NotAuthenticated/NotAuthenticated';
-// import OtherPersonalProfileReviews from '../OtherProfile/Reviews/OtherPersonalProfileReviews';
-// import PersonalProfileReview from './Reviews/PersonalProfileReviews';
+import React from "react";
+import { useParams } from "react-router-dom";
+import { checkProfileOwner, findUser } from "../../GraphQLQueries/queries";
+import { Loading } from "../Loading/Loading";
+import NotAuthenticated from "../NotAuthenticated/NotAuthenticated";
+import OtherPersonalProfileReviews from "../OtherProfile/Reviews/OtherPersonalProfileReviews";
+import PersonalProfileReview from "./Reviews/PersonalProfileReviews";
 
-// //Landing page for profile. Checks the info given in the browser cookie (Generated during login) to see if the profile you are viewing is yours
+//Landing page for profile. Checks the info given in the browser cookie (Generated during login) to see if the profile you are viewing is yours
 
-// function ProfileReviewsLanding(){
-//   const [data, setData] = React.useState("")
-//   const [owner, setOwner] = React.useState(false);
-//   const {id} = useParams();
-//   //Upon mounting component, data retrieved and sent down to profile component.
-//   React.useEffect(()=>{
-//     const apiCall = async () => {
-//       const user = await findUser(id)
-//       setData(user.data.data.userProfileInfo)
-//       const check = await checkProfileOwner(id)
-//       setOwner(check.data.data.checkProfileOwner)
-//     };
+function ProfileReviewsLanding() {
+  const [data, setData] = React.useState("");
+  const [owner, setOwner] = React.useState(false);
+  const { id } = useParams();
+  //Upon mounting component, data retrieved and sent down to profile component.
+  React.useEffect(() => {
+    const apiCall = async () => {
+      const user = await findUser(id);
+      setData(user.data.data.userProfileInfo);
+      const check = await checkProfileOwner(id);
+      setOwner(check.data.data.checkProfileOwner);
+    };
 
-//     apiCall()
-//   },[id])
+    apiCall();
+  }, [id]);
 
-//   if (data === "Not authenticated" || data === null) {
-//     return <NotAuthenticated />;
-//   } else if (data === "") {
-//     return <Loading />;
-//   }
+  if (data === "Not authenticated" || data === null) {
+    return <NotAuthenticated />;
+  } else if (data === "") {
+    return <Loading />;
+  }
 
-//     return owner ? <PersonalProfileReview user={data}/> : <OtherPersonalProfileReviews user={data}/>
+  return owner ? (
+    <PersonalProfileReview user={data} />
+  ) : (
+    <OtherPersonalProfileReviews user={data} />
+  );
+}
 
-// }
-
-// export default ProfileReviewsLanding;
+export default ProfileReviewsLanding;
