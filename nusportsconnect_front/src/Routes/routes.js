f96cbb4c5ef7c2678fd7e3aaefc2b7afb0b2e5c3
unknown <ezekielang@protonmail.com>
2022-07-25 21:52:01 +0800

    deployment

--- nusportsconnect_front/src/Routes/routes.js
+++ nusportsconnect_front/src/Routes/routes.js
@@ -1,9 +1,9 @@
 //Add routes here for organisational purposes. Routes will change once we publish to a hosting service.
 //Production
-export const refreshTokenRoute = "/refresh_token";
-export const reqOriginRoute = "http://localhost:3000/";
+// export const refreshTokenRoute = "/refresh_token";
+// export const reqOriginRoute = "http://localhost:3000/";
 
 //Deployment
 
-// export const refreshTokenRoute = "https://nusportsconnect.herokuapp.com/refresh_token";
-// export const reqOriginRoute = "https://nusportsconnect.herokuapp.com/";
+export const refreshTokenRoute = "https://nusportsconnect.herokuapp.com/refresh_token";
+export const reqOriginRoute = "https://nusportsconnect.herokuapp.com/";
